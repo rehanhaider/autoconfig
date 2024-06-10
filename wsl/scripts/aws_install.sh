e8b327ae8524461bcdbc76f01e7fb5b94dbe0b63
Rehan Haider <email@rehanhaider.com>
2024-06-10 16:29:59 +0000

    ✨ FEAT:  AWS credentials symlink with windows system

--- wsl/scripts/aws_install.sh
+++ wsl/scripts/aws_install.sh
@@ -12,7 +12,25 @@ install_aws_cli() {
     fi
 }
 
+link_win_credentials() {
+    WIN_USER=$(powershell.exe 'Write-Host $env:UserName')
+
+    WIN_AWS_PATH=$(find /mnt/c/Users -maxdepth 1 -type d -regextype posix-extended -iregex "/mnt/c/Users/${WIN_USER}")
+
+    if [ -d "{$WIN_AWS_PATH}/.aws" ]; then
+        PASS "AWS credentials already linked. Skipping..."
+    else
+        #RUN "Link AWS credentials" "ln -s '/mnt/c/Users/${USER}/.aws' '${HOME}/.aws'"
+        echo "Linking AWS credentials..."
+    fi
+}
+
 DELIM "Installing AWS CLI..."
 PROMPT "Install AWS CLI" install_aws_cli
 NEWLINE
 PASS "Installed AWS CLI successfully."
+
+DELIM "Linking AWS credentials..."
+PROMPT "Link AWS credentials" link_win_credentials
+NEWLINE
+PASS "Linked AWS credentials successfully."
