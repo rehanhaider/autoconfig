eea3f49b4241516376e84f29058a2262452635d3
Rehan Haider <email@rehanhaider.com>
2024-06-10 14:35:29 +0000

    ✨ FEAT:  Mise installation and langauge installation

--- wsl/scripts/install_mise.sh
+++ wsl/scripts/install_mise.sh
@@ -12,11 +12,10 @@ install_mise() {
 install_mise_langs() {
     ## Check if node is already installed
     RUN "Install latest Node.js & NPM" "${HOME}/.local/bin/mise use --global node@latest"
-    RUN "Set Mise to compile python" "${HOME}/.local/bin/mise settings set python_compile 1"
-    RUN "Install latest Node.js & NPM" "${HOME}/.local/bin/mise use --global python@3.12"
+    RUN "Install latest Python" "${HOME}/.local/bin/mise use --global python@3.12"
 }
 
-## Prompt the user to configure .bashrc
+# MAIN
 DELIM "Installing mise..."
 PROMPT "Installing mise" install_mise
 NEWLINE
