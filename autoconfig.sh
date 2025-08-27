fb7b4f9b20d573c9cc35b51d273bd51b04b0d6c3
Rehan Haider <email@rehanhaider.com>
2025-08-27 17:52:53 +0000

    FIX:  fixed docker command

--- autoconfig.sh
+++ autoconfig.sh
@@ -57,14 +57,14 @@ rprint "${COLOR_WARN}" "${DELIMITER}"
 
 
 
-source "${SCRIPT_DIR}/install_packages.sh"
-source "${SCRIPT_DIR}/prepare_wac.sh"
-source "${SCRIPT_DIR}/install_mise.sh"
-source "${SCRIPT_DIR}/install_uv.sh"
-source "${SCRIPT_DIR}/bash_config.sh"
-source "${SCRIPT_DIR}/omp_config.sh"
-source "${SCRIPT_DIR}/git_config.sh"
-source "${SCRIPT_DIR}/aws_install.sh"
+# source "${SCRIPT_DIR}/install_packages.sh"
+# source "${SCRIPT_DIR}/prepare_wac.sh"
+# source "${SCRIPT_DIR}/install_mise.sh"
+# source "${SCRIPT_DIR}/install_uv.sh"
+# source "${SCRIPT_DIR}/bash_config.sh"
+# source "${SCRIPT_DIR}/omp_config.sh"
+# source "${SCRIPT_DIR}/git_config.sh"
+# source "${SCRIPT_DIR}/aws_install.sh"
 source "${SCRIPT_DIR}/docker_install.sh"
 
 NEWLINE
--- wsl/scripts/docker_install.sh
+++ wsl/scripts/docker_install.sh
@@ -6,15 +6,12 @@ update_apt() {
         RUN "Install keyrings" "sudo install -m 0755 -d /etc/apt/keyrings"
         RUN "Install Docker GPG key" "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc"
         RUN "Install Docker GPG key" "sudo chmod a+r /etc/apt/keyrings/docker.asc"
-        RUN "Add Docker repository" "echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"""
+        RUN "Add Docker repository" "echo deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo ${UBUNTU_CODENAME:-$VERSION_CODENAME}) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
         RUN "Update Apt" "sudo apt update"
 }
 
 install_docker() {
-    RUN "Install Docker" "sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+    RUN "Install Docker" "sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
 }
 
 
